4c0d406aaed5814685bab509ea3b7a1b8d9b5421
Shilpa S <shilpa.shravge@numito.com>
2026-02-18 14:16:51 +0000

    created 2nd commit for cherry pick understanding

--- src/main/java/pages/CherryPickPage.java
+++ src/main/java/pages/CherryPickPage.java
@@ -8,5 +8,10 @@ public class CherryPickPage {
 		System.out.println("This method is created in commit1");
 	}
 	
+	public void display2()
+	{
+		System.out.println("This method is created in commit2");
+	}
+	
 	
 }
